579de137de31388c92760a6dcdbca20e5f9b4d27
Aluizio Neto <aluizioneto.dev@gmail.com>
2025-12-18 23:00:04 -0300

    feat: register page and home following requirements

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,14 +3,19 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login';
 import { Home } from './pages/home/home';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: LoginComponent,
   },
   {
     path: 'home',
     component: Home
+  },
+  {
+    path: 'register',
+    component: Register
   }
 ];
